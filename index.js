77e7d63b6c1ae96cdf27ab53fb3534117abf3a9b
Jess Pesale <jess_pesale@aol.com>
2021-07-20 20:18:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  docLoaded()
+}); 
+
+function docLoaded(){
+    let element = document.getElementById("text")
+    element.innerHTML = "This is really cool!"
+}
